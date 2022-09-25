432dd9f73b623d9150ced3cab2ce783b1e1aac4e
jsdno0 <csdnum0@gmail.com>
2022-09-25 15:54:01 +0900

    cc

--- test.js
+++ test.js
@@ -15,6 +15,7 @@ const watcher = chokidar.watch('watch-folder', {
 })
 
 const state = {
+    startOver: false,
     unlinks: {
         switch: false,
         list: [],
@@ -23,7 +24,7 @@ const state = {
     add: {
         switch: false,
         list: [],
-        cnt:0
+        // cnt:0
     },
     change: {
         switch: false,
@@ -32,42 +33,83 @@ const state = {
     }
 }
 
+let loadPath = [];
+let cnt = 0;
+let loop = 0;
 watcher.on('ready', async ()=> {
     console.log('Ready to watch files')
-    await recursiveFile('C:/Users/jsd/programming/nodejs/chokidar-tuto/watch-folder', 'change')
-
+    state.startOver = true;
     console.log(state)
-}).on('add', path => {
-    // console.log(watcher)
-    console.log(path, 'File is added')
+}).on('add', async (path) => {
+    if(state.startOver === true){
+        if(state.add.switch === false) {
+            state.add.list = new Array();
+            await recursiveFile('C:/Users/jsd/programming/nodejs/chokidar-tuto/watch-folder', 'add')
+            cnt = state.add.list.length - loadPath.length
+            state.add.switch = true
+        }
+        await funcLoadPath(path, 'add')
+        console.log('debug2 : ', loop, cnt)
+        console.log('debug3-1', state.add.list)
+        console.log('debug3-2', loadPath)
+        if(
+            loop === (cnt-1)
+            ){
+            loop = 0
+            cnt = 0
+            state.add.list = new Array()
+            state.add.switch = false
+            console.log('add program update')
+        } else {
+            loop++;
+        }
+    } else {
+        funcLoadPath(path, 'add')
+        console.log(path, 'File is added')
+    }
 })
-// .on('raw', (events, path, details) => {
-    // console.log(path, 'File is raw ' + events)
-// })
-.on('unlink', path => {
-    if(state.unlinks.switch === false){
-        const unlinks = Object.fromEntries(watcher._pendingUnlinks);
-        const keys = Object.keys(unlinks)
-        state.unlinks.list = keys;
-        state.unlinks.switch = true;
-        console.log('removed', state.unlinks.list)
+.on('unlink', async (path) => {
+    if(state.startOver === true) {
+        if(state.unlinks.switch === false){
+            const unlinks = Object.fromEntries(watcher._pendingUnlinks);
+            const keys = Object.keys(unlinks)
+            state.unlinks.list = keys;
+            state.unlinks.switch = true;
+            // console.log('removed', state.unlinks.list)
+        }
+        state.unlinks.cnt++;
+        console.log('debug3 : ' + state.unlinks.cnt + '/' + state.unlinks.list.length)
+        if(state.unlinks.cnt === state.unlinks.list.length){
+            state.unlinks.list = new Array();
+            state.unlinks.switch = false;
+            state.unlinks.cnt = 0;
+            console.log('unlink program update!')
+        }
+        await funcLoadPath(path, 'unlink')
+    } else {
+        funcLoadPath(path, 'unlink')
+        console.log(path, 'File is removed')
     }
-    state.unlinks.cnt++;
-    // console.log(state.unlinks.cnt + '/' + state.unlinks.list.length)
-    if(state.unlinks.cnt === state.unlinks.list.length){
-        state.unlinks.list = new Array();
-        state.unlinks.switch = false;
-        state.unlinks.cnt = 0;
-        console.log('program update!')
-    }
-    console.log(path, 'File is removed');
 }).on('change', path => {
     console.log(path, 'File is changed');
 }).on('error', error => {
     console.log(error)
 });
 
-
+async function funcLoadPath(path, type){
+    switch(type){
+        case 'add':
+            loadPath.push(path); break;
+        case 'unlink':
+            loadPath = loadPath.filter((v,i,a)=>{
+                return v !== path
+            }); 
+            break;
+    }
+    loadPath = loadPath.filter((v,i,a) => {
+        return a.indexOf(v) === i
+    })
+}
 async function recursiveFile(dir, type){
     const fileList = fs.readdirSync(dir)
         for(let i=0; i<fileList.length; i++) {
@@ -85,5 +127,4 @@ async function recursiveFile(dir, type){
             }
 
         }
-
 }
